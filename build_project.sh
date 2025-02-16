766bff84cb6ca1e20dcb917e65c09380cae70b5f
Steven Bautista <stevenmb1995@gmail.com>
2025-02-15 19:39:48 -0600

    added a docs folder and .gitignore

--- build_project.sh
+++ build_project.sh
@@ -12,3 +12,10 @@ emcmake cmake -B build -S .
 
 # Build with emmake
 cd build && emmake make
+
+# Copy the generated files to the docs folder
+cd ..
+mkdir -p docs
+cp build/sdl_wasm_project.html docs/index.html
+cp build/sdl_wasm_project.js docs/
+cp build/sdl_wasm_project.wasm docs/
